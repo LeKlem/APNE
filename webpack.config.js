abd4512e25b1b40459c6a1db165a5bea5148a567
Tek <lbplanetcraft@gmail.com>
2021-10-26 12:04:43 +0200

    header news

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,10 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
+    .addEntry('header', './assets/styles/header.css')
+
+
+
 ;
 
 module.exports = Encore.getWebpackConfig();
