3e8fe968fdbde28a482210aeaf80c7b4f34c4d6d
Tek <lbplanetcraft@gmail.com>
2021-11-23 12:16:22 +0100

    working admin product

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            IntegerField::new('price'),
+            TextEditorField::new('description'),
+            Field::new('imageFile')->setFormType(VichImageType::class),
+        ];
+    }
+    
+}
--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ShopRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShopController extends AbstractController
@@ -16,6 +20,7 @@ class ShopController extends AbstractController
         $products = $this->getDoctrine()
             ->getRepository(Product::class)
             ->findAll();
+
         return $this->render('Shop/shop.html.twig',[
             'products' => $products
     ]);
